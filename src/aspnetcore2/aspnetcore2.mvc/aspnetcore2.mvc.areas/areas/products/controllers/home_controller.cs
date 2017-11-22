75f568eea6b0b6368c125c5a74af4ae03306016d
Developer GNU <adorno@protonmail.com>
2017-11-22 14:48:43 -0200

    Areas Added

---
+++ src/aspnetcore2/aspnetcore2.mvc/aspnetcore2.mvc.areas/areas/products/controllers/home_controller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aspnet.Core.Areas.Areas.Products.Controllers
+{
+    [Area("Products")]
+    public class HomeController : Controller
+    {
+        // GET: /Products/Home/Index
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /Products/Home/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
